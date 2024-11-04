394985d2063bb84baafe81976a7496b846edcdef
ArmandMKU <armandmku@talk21.com>
2024-11-04 01:25:58 +0000

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
 import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
-{
+export default {
+  // config options
   plugins: [
     // ...
     chunkSplitPlugin(),
-  ];
-}
+  ],
+};
