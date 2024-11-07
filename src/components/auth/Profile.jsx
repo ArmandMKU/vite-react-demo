c090e70715add9b8a4d8af5ccff72ef23f7a6137
ArmandMKU <armandmku@talk21.com>
2024-11-07 00:20:42 +0000

    replace moment by dayjs

--- src/components/auth/Profile.jsx
+++ src/components/auth/Profile.jsx
@@ -1,207 +1,241 @@
-import React, { useEffect, useState } from "react"
-import { deleteUser, getBookingsByUserId, getUser } from "../utils/ApiFunctions"
-import { useNavigate } from "react-router-dom"
-import moment from "moment"
+import React, { useEffect, useState } from "react";
+import {
+  deleteUser,
+  getBookingsByUserId,
+  getUser,
+} from "../utils/ApiFunctions";
+import { useNavigate } from "react-router-dom";
+// import moment from "moment"
+import dayjs from "dayjs";
 
 const Profile = () => {
-	const [user, setUser] = useState({
-		id: "",
-		email: "",
-		firstName: "",
-		lastName: "",
-		roles: [{ id: "", name: "" }]
-	})
+  const [user, setUser] = useState({
+    id: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+    roles: [{ id: "", name: "" }],
+  });
 
-	const [bookings, setBookings] = useState([
-		{
-			id: "",
-			room: { id: "", roomType: "" },
-			checkInDate: "",
-			checkOutDate: "",
-			bookingConfirmationCode: ""
-		}
-	])
-	const [message, setMessage] = useState("")
-	const [errorMessage, setErrorMessage] = useState("")
-	const navigate = useNavigate()
+  const [bookings, setBookings] = useState([
+    {
+      id: "",
+      room: { id: "", roomType: "" },
+      checkInDate: "",
+      checkOutDate: "",
+      bookingConfirmationCode: "",
+    },
+  ]);
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
-	const userId = localStorage.getItem("userId")
-	const token = localStorage.getItem("token")
+  const userId = localStorage.getItem("userId");
+  const token = localStorage.getItem("token");
 
-	useEffect(() => {
-		const fetchUser = async () => {
-			try {
-				const userData = await getUser(userId, token)
-				setUser(userData)
-			} catch (error) {
-				console.error(error)
-			}
-		}
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userData = await getUser(userId, token);
+        setUser(userData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-		fetchUser()
-	}, [userId])
+    fetchUser();
+  }, [userId]);
 
-	useEffect(() => {
-		const fetchBookings = async () => {
-			try {
-				const response = await getBookingsByUserId(userId, token)
-				setBookings(response)
-			} catch (error) {
-				console.error("Erreur lors de la récupération des réservations:", error.message)
-				setErrorMessage(error.message)
-			}
-		}
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const response = await getBookingsByUserId(userId, token);
+        setBookings(response);
+      } catch (error) {
+        console.error(
+          "Erreur lors de la récupération des réservations:",
+          error.message
+        );
+        setErrorMessage(error.message);
+      }
+    };
 
-		fetchBookings()
-	}, [userId])
+    fetchBookings();
+  }, [userId]);
 
-	const handleDeleteAccount = async () => {
-		const confirmed = window.confirm(
-			"Etes-vous sûr de vouloir supprimer votre compte ? Cette action ne peut pas être annulée."
-		)
-		if (confirmed) {
-			await deleteUser(userId)
-				.then((response) => {
-					setMessage(response.data)
-					localStorage.removeItem("token")
-					localStorage.removeItem("userId")
-					localStorage.removeItem("userRole")
-					navigate("/")
-					window.location.reload()
-				})
-				.catch((error) => {
-					setErrorMessage(error.data)
-				})
-		}
-	}
+  const handleDeleteAccount = async () => {
+    const confirmed = window.confirm(
+      "Etes-vous sûr de vouloir supprimer votre compte ? Cette action ne peut pas être annulée."
+    );
+    if (confirmed) {
+      await deleteUser(userId)
+        .then((response) => {
+          setMessage(response.data);
+          localStorage.removeItem("token");
+          localStorage.removeItem("userId");
+          localStorage.removeItem("userRole");
+          navigate("/");
+          window.location.reload();
+        })
+        .catch((error) => {
+          setErrorMessage(error.data);
+        });
+    }
+  };
 
-	return (
-		<div className="container">
-			{errorMessage && <p className="text-danger">{errorMessage}</p>}
-			{message && <p className="text-danger">{message}</p>}
-			{user ? (
-				<div className="card p-5 mt-5" style={{ backgroundColor: "whitesmoke" }}>
-					<h4 className="card-title text-center">informations de l'utilisateur</h4>
-					<div className="card-body">
-						<div className="col-md-10 mx-auto">
-							<div className="card mb-3 shadow">
-								<div className="row g-0">
-									<div className="col-md-2">
-										<div className="d-flex justify-content-center align-items-center mb-4">
-											<img
-												src="https://themindfulaimanifesto.org/wp-content/uploads/2020/09/male-placeholder-image.jpeg"
-												alt="Profile"
-												className="rounded-circle"
-												style={{ width: "150px", height: "150px", objectFit: "cover" }}
-											/>
-										</div>
-									</div>
+  return (
+    <div className="container">
+      {errorMessage && <p className="text-danger">{errorMessage}</p>}
+      {message && <p className="text-danger">{message}</p>}
+      {user ? (
+        <div
+          className="card p-5 mt-5"
+          style={{ backgroundColor: "whitesmoke" }}
+        >
+          <h4 className="card-title text-center">
+            informations de l'utilisateur
+          </h4>
+          <div className="card-body">
+            <div className="col-md-10 mx-auto">
+              <div className="card mb-3 shadow">
+                <div className="row g-0">
+                  <div className="col-md-2">
+                    <div className="d-flex justify-content-center align-items-center mb-4">
+                      <img
+                        src="https://themindfulaimanifesto.org/wp-content/uploads/2020/09/male-placeholder-image.jpeg"
+                        alt="Profile"
+                        className="rounded-circle"
+                        style={{
+                          width: "150px",
+                          height: "150px",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </div>
+                  </div>
 
-									<div className="col-md-10">
-										<div className="card-body">
-											<div className="form-group row">
-												<label className="col-md-2 col-form-label fw-bold">ID:</label>
-												<div className="col-md-10">
-													<p className="card-text">{user.id}</p>
-												</div>
-											</div>
-											<hr />
+                  <div className="col-md-10">
+                    <div className="card-body">
+                      <div className="form-group row">
+                        <label className="col-md-2 col-form-label fw-bold">
+                          ID:
+                        </label>
+                        <div className="col-md-10">
+                          <p className="card-text">{user.id}</p>
+                        </div>
+                      </div>
+                      <hr />
 
-											<div className="form-group row">
-												<label className="col-md-2 col-form-label fw-bold">Prénom:</label>
-												<div className="col-md-10">
-													<p className="card-text">{user.firstName}</p>
-												</div>
-											</div>
-											<hr />
+                      <div className="form-group row">
+                        <label className="col-md-2 col-form-label fw-bold">
+                          Prénom:
+                        </label>
+                        <div className="col-md-10">
+                          <p className="card-text">{user.firstName}</p>
+                        </div>
+                      </div>
+                      <hr />
 
-											<div className="form-group row">
-												<label className="col-md-2 col-form-label fw-bold">Nom:</label>
-												<div className="col-md-10">
-													<p className="card-text">{user.lastName}</p>
-												</div>
-											</div>
-											<hr />
+                      <div className="form-group row">
+                        <label className="col-md-2 col-form-label fw-bold">
+                          Nom:
+                        </label>
+                        <div className="col-md-10">
+                          <p className="card-text">{user.lastName}</p>
+                        </div>
+                      </div>
+                      <hr />
 
-											<div className="form-group row">
-												<label className="col-md-2 col-form-label fw-bold">Email:</label>
-												<div className="col-md-10">
-													<p className="card-text">{user.email}</p>
-												</div>
-											</div>
-											<hr />
+                      <div className="form-group row">
+                        <label className="col-md-2 col-form-label fw-bold">
+                          Email:
+                        </label>
+                        <div className="col-md-10">
+                          <p className="card-text">{user.email}</p>
+                        </div>
+                      </div>
+                      <hr />
 
-											<div className="form-group row">
-												<label className="col-md-2 col-form-label fw-bold">Roles:</label>
-												<div className="col-md-10">
-													<ul className="list-unstyled">
-														{user.roles.map((role) => (
-															<li key={role.id} className="card-text">
-																{role.name}
-															</li>
-														))}
-													</ul>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
+                      <div className="form-group row">
+                        <label className="col-md-2 col-form-label fw-bold">
+                          Roles:
+                        </label>
+                        <div className="col-md-10">
+                          <ul className="list-unstyled">
+                            {user.roles.map((role) => (
+                              <li key={role.id} className="card-text">
+                                {role.name}
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-							<h4 className="card-title text-center">Historique des réservations</h4>
+              <h4 className="card-title text-center">
+                Historique des réservations
+              </h4>
 
-							{bookings.length > 0 ? (
-								<table className="table table-bordered table-hover shadow">
-									<thead>
-										<tr>
-											<th scope="col">réservation ID</th>
-											<th scope="col">Chambre ID</th>
-											<th scope="col">Type de chambre</th>
-											<th scope="col">Arrivée</th>
-											<th scope="col">Départ</th>
-											<th scope="col">Code Confirmation</th>
-											<th scope="col">Statut</th>
-										</tr>
-									</thead>
-									<tbody>
-										{bookings.map((booking, index) => (
-											<tr key={index}>
-												<td>{booking.id}</td>
-												<td>{booking.room.id}</td>
-												<td>{booking.room.roomType}</td>
-												<td>
-													{moment(booking.checkInDate).subtract(1, "month").format("MMM Do, YYYY")}
-												</td>
-												<td>
-													{moment(booking.checkOutDate)
-														.subtract(1, "month")
-														.format("MMM Do, YYYY")}
-												</td>
-												<td>{booking.bookingConfirmationCode}</td>
-												<td className="text-success">En cours</td>
-											</tr>
-										))}
-									</tbody>
-								</table>
-							) : (
-								<p>Vous n'avez pas encore effectué de réservation.</p>
-							)}
+              {bookings.length > 0 ? (
+                <table className="table table-bordered table-hover shadow">
+                  <thead>
+                    <tr>
+                      <th scope="col">réservation ID</th>
+                      <th scope="col">Chambre ID</th>
+                      <th scope="col">Type de chambre</th>
+                      <th scope="col">Arrivée</th>
+                      <th scope="col">Départ</th>
+                      <th scope="col">Code Confirmation</th>
+                      <th scope="col">Statut</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {bookings.map((booking, index) => (
+                      <tr key={index}>
+                        <td>{booking.id}</td>
+                        <td>{booking.room.id}</td>
+                        <td>{booking.room.roomType}</td>
+                        <td>
+                          {dayjs(booking.checkInDate)
+                            .subtract(1, "month")
+                            .format("MMM Do, YYYY")}
+                        </td>
+                        <td>
+                          {dayjs(booking.checkOutDate)
+                            .subtract(1, "month")
+                            .format("MMM Do, YYYY")}
+                        </td>
+                        <td>{booking.bookingConfirmationCode}</td>
+                        <td className="text-success">En cours</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              ) : (
+                <p>Vous n'avez pas encore effectué de réservation.</p>
+              )}
 
-							<div className="d-flex justify-content-center">
-								<div className="mx-2">
-									<button className="btn btn-danger btn-sm" onClick={handleDeleteAccount}>
-										Cloturer le compte
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			) : (
-				<p>Chargement des données utilisateur...</p>
-			)}
-		</div>
-	)
-}
+              <div className="d-flex justify-content-center">
+                <div className="mx-2">
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={handleDeleteAccount}
+                  >
+                    Cloturer le compte
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <p>Chargement des données utilisateur...</p>
+      )}
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
--- src/components/booking/BookingSummary.jsx
+++ src/components/booking/BookingSummary.jsx
@@ -1,96 +1,106 @@
-import React, { useState, useEffect } from "react"
-import moment from "moment"
-import Button from "react-bootstrap/Button"
-import { useNavigate } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+// import moment from "moment"
+import dayjs from "dayjs";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const BookingSummary = ({ booking, payment, isFormValid, onConfirm }) => {
-	const checkInDate = moment(booking.checkInDate)
-	const checkOutDate = moment(booking.checkOutDate)
-	const numberOfDays = checkOutDate.diff(checkInDate, "days")
-	const [isBookingConfirmed, setIsBookingConfirmed] = useState(false)
-	const [isProcessingPayment, setIsProcessingPayment] = useState(false)
-	const navigate = useNavigate()
+  // const checkInDate = moment(booking.checkInDate)
+  // const checkOutDate = moment(booking.checkOutDate)
+  const checkInDate = dayjs(booking.checkInDate);
+  const checkOutDate = dayjs(booking.checkOutDate);
+  const numberOfDays = checkOutDate.diff(checkInDate, "day");
+  const [isBookingConfirmed, setIsBookingConfirmed] = useState(false);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+  const navigate = useNavigate();
 
-	const handleConfirmBooking = () => {
-		setIsProcessingPayment(true)
-		setTimeout(() => {
-			setIsProcessingPayment(false)
-			setIsBookingConfirmed(true)
-			onConfirm()
-		}, 3000)
-	}
+  const handleConfirmBooking = () => {
+    setIsProcessingPayment(true);
+    setTimeout(() => {
+      setIsProcessingPayment(false);
+      setIsBookingConfirmed(true);
+      onConfirm();
+    }, 3000);
+  };
 
-	useEffect(() => {
-		if (isBookingConfirmed) {
-			navigate("/booking-success")
-		}
-	}, [isBookingConfirmed, navigate])
+  useEffect(() => {
+    if (isBookingConfirmed) {
+      navigate("/booking-success");
+    }
+  }, [isBookingConfirmed, navigate]);
 
-	return (
-		<div className="row">
-			<div className="col-md-6"></div>
-			<div className="card card-body mt-5">
-				<h4 className="card-title hotel-color">Résumé de la réservation</h4>
-				<p>
-					Nom: <strong>{booking.guestFullName}</strong>
-				</p>
-				<p>
-					Email: <strong>{booking.guestEmail}</strong>
-				</p>
-				<p>
-					Arrivée: <strong>{moment(booking.checkInDate).format("MMM Do YYYY")}</strong>
-				</p>
-				<p>
-					Départ: <strong>{moment(booking.checkOutDate).format("MMM Do YYYY")}</strong>
-				</p>
-				<p>
-					Nombre de jours réservés: <strong>{numberOfDays}</strong>
-				</p>
+  return (
+    <div className="row">
+      <div className="col-md-6"></div>
+      <div className="card card-body mt-5">
+        <h4 className="card-title hotel-color">Résumé de la réservation</h4>
+        <p>
+          Nom: <strong>{booking.guestFullName}</strong>
+        </p>
+        <p>
+          Email: <strong>{booking.guestEmail}</strong>
+        </p>
+        <p>
+          Arrivée:{" "}
+          <strong>{dayjs(booking.checkInDate).format("MMM Do YYYY")}</strong>
+          {/* <strong>{moment(booking.checkInDate).format("MMM Do YYYY")}</strong> */}
+        </p>
+        <p>
+          Départ:{" "}
+          <strong>{dayjs(booking.checkOutDate).format("MMM Do YYYY")}</strong>
+          {/* <strong>{moment(booking.checkOutDate).format("MMM Do YYYY")}</strong> */}
+        </p>
+        <p>
+          Nombre de jours réservés: <strong>{numberOfDays}</strong>
+        </p>
 
-				<div>
-					<h5 className="hotel-color">Nombre de personnes</h5>
-					<strong>
-						Adult{booking.numOfAdults > 1 ? "s" : ""} : {booking.numOfAdults}
-					</strong>
-					<strong>
-						<p>Enfants : {booking.numOfChildren}</p>
-					</strong>
-				</div>
+        <div>
+          <h5 className="hotel-color">Nombre de personnes</h5>
+          <strong>
+            Adult{booking.numOfAdults > 1 ? "s" : ""} : {booking.numOfAdults}
+          </strong>
+          <strong>
+            <p>Enfants : {booking.numOfChildren}</p>
+          </strong>
+        </div>
 
-				{payment > 0 ? (
-					<>
-						<p>
-							Total à payer: <strong>${payment}</strong>
-						</p>
+        {payment > 0 ? (
+          <>
+            <p>
+              Total à payer: <strong>${payment}</strong>
+            </p>
 
-						{isFormValid && !isBookingConfirmed ? (
-							<Button variant="success" onClick={handleConfirmBooking}>
-								{isProcessingPayment ? (
-									<>
-										<span
-											className="spinner-border spinner-border-sm mr-2"
-											role="status"
-											aria-hidden="true"></span>
-										Réservation confirmée, redirection vers le paiement...
-									</>
-								) : (
-									"Confirmer la réservation et procéder au paiement"
-								)}
-							</Button>
-						) : isBookingConfirmed ? (
-							<div className="d-flex justify-content-center align-items-center">
-								<div className="spinner-border text-primary" role="status">
-									<span className="sr-only">Chargement...</span>
-								</div>
-							</div>
-						) : null}
-					</>
-				) : (
-					<p className="text-danger">La date de départ doit être postérieure à la date d'arrivée.</p>
-				)}
-			</div>
-		</div>
-	)
-}
+            {isFormValid && !isBookingConfirmed ? (
+              <Button variant="success" onClick={handleConfirmBooking}>
+                {isProcessingPayment ? (
+                  <>
+                    <span
+                      className="spinner-border spinner-border-sm mr-2"
+                      role="status"
+                      aria-hidden="true"
+                    ></span>
+                    Réservation confirmée, redirection vers le paiement...
+                  </>
+                ) : (
+                  "Confirmer la réservation et procéder au paiement"
+                )}
+              </Button>
+            ) : isBookingConfirmed ? (
+              <div className="d-flex justify-content-center align-items-center">
+                <div className="spinner-border text-primary" role="status">
+                  <span className="sr-only">Chargement...</span>
+                </div>
+              </div>
+            ) : null}
+          </>
+        ) : (
+          <p className="text-danger">
+            La date de départ doit être postérieure à la date d'arrivée.
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default BookingSummary
+export default BookingSummary;
