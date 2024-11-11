54495c7e80aa8044912d1439b712715b1362d80b
ArmandMKU <armandmku@talk21.com>
2024-11-11 00:30:05 +0000

    change locale

--- src/components/common/DateSlider.jsx
+++ src/components/common/DateSlider.jsx
@@ -1,39 +1,46 @@
-import React, { useState } from "react"
-import "react-date-range/dist/styles.css"
-import "react-date-range/dist/theme/default.css"
-import { DateRangePicker } from "react-date-range"
+import React, { useState } from "react";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
+import locale from "date-fns/locale/fr-FR";
 
 const DateSlider = ({ onDateChange, onFilterChange }) => {
-	const [dateRange, setDateRange] = useState({
-		startDate: undefined,
-		endDate: undefined,
-		key: "selection"
-	})
+  const [dateRange, setDateRange] = useState({
+    startDate: undefined,
+    endDate: undefined,
+    key: "selection",
+  });
 
-	const handleSelect = (ranges) => {
-		setDateRange(ranges.selection)
-		onDateChange(ranges.selection.startDate, ranges.selection.endDate)
-		onFilterChange(ranges.selection.startDate, ranges.selection.endDate)
-	}
+  const handleSelect = (ranges) => {
+    setDateRange(ranges.selection);
+    onDateChange(ranges.selection.startDate, ranges.selection.endDate);
+    onFilterChange(ranges.selection.startDate, ranges.selection.endDate);
+  };
 
-	const handleClearFilter = () => {
-		setDateRange({
-			startDate: undefined,
-			endDate: undefined,
-			key: "selection"
-		})
-		onDateChange(null, null)
-		onFilterChange(null, null)
-	}
-	return (
-		<>
-			<h5>Filtrer les réservations par date</h5>
-			<DateRangePicker ranges={[dateRange]} onChange={handleSelect} className="mb-4" />
-			<button className="btn btn-secondary" onClick={handleClearFilter}>
-				Enlever le filtre
-			</button>
-		</>
-	)
-}
+  const handleClearFilter = () => {
+    setDateRange({
+      startDate: undefined,
+      endDate: undefined,
+      key: "selection",
+    });
+    onDateChange(null, null);
+    onFilterChange(null, null);
+  };
+  return (
+    <>
+      <h5>Filtrer les réservations par date</h5>
+      <LocalizationProvider dateAdapter={AdapterDateFns} locale={locale}>
+        <DateRangePicker
+          ranges={[dateRange]}
+          onChange={handleSelect}
+          className="mb-4"
+        />
+      </LocalizationProvider>
+      <button className="btn btn-secondary" onClick={handleClearFilter}>
+        Enlever le filtre
+      </button>
+    </>
+  );
+};
 
-export default DateSlider
+export default DateSlider;
